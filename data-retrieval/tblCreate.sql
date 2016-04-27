b021f43feadff6c7d43e16b1211dd88891edf667
Ryan Matthews <Ryan.matthews@colorado.edu>
2016-04-26 20:33:39 -0600

    Update tblCreate.sql

--- data-retrieval/tblCreate.sql
+++ data-retrieval/tblCreate.sql
@@ -1,2 +1,2 @@
-create table beers(name char(40) primary key, ABV integer, IBU integer);
-insert into beer values ('
+create table beers(name char(40) primary key, ABV float, IBU float);
+insert into beer values ('Heineken', 4.4, 18.0);
